b00e54ea4644c3bc4ee54f38f9b8c9e152323144
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-18 17:54:36 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,25 +7,19 @@ class Actor < ActiveRecord::Base
    end
 
    def list_roles
-      #   all_characters=characters.select {|char| char.actor_id = self.id}
-      # all_shows=Show.all.select {|show| show.id == all_characters.show_id}
-      # "#{all_characters.name} - #{all_shows.name}"
-      
-         result=[]
-         for character in characters
-            for show in shows
-               if character.show_id == show.id
-                result<<"#{character.name} - #{show.name}"
-               end
-            end
+         # result=[]
+         # for character in characters
+           
+         #       if character.show_id == show.id
+         #        result<<"#{character.name} - #{show.name}"
+         #       end
+         #    end
+         # end
+
+         # result
+
+         characters.map do |char|
+            "#{char.name} - #{char.show.name}"
          end
-
-         result
-         #  Write a method in the Actor class, #list_roles, that lists all of the characters that actor has
-         # select actor.name, show.name 
-         # from actors, characters, shows
-         # where actors.id = characters.actor_id and characters.show_id=shows.id
-
-
    end
 end
