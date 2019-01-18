453a32dad6aae7e8007031b7cbfa8dc17152caa8
Cristian Leiva <cleiva94.cl@gmail.com>
2019-01-18 15:41:40 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+
+    def say_that_thing_you_say
+        name = Character.pluck("name").pop()
+        catchphrase = Character.pluck("catchphrase").pop()
+        "#{name} always says: #{catchphrase}"
+    end
 end
+
+    
