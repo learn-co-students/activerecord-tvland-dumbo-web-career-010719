28eb497479d26cc78181907d98cc963f6fdfb16a
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-18 14:15:06 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+   has_many :characters
+   has_many :shows, through: :characters
+
+   def full_name
+    "#{self.first_name} #{self.last_name}"
+   end
+
+   def list_roles
+        name=characters.find {|char| char.actor_id = self.id}
+        show_name=Show.all.find {|show| show.id == name.show_id}
+         "#{name.name} - #{show_name.name}"
+   end
 end
